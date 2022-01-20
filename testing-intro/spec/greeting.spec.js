b05b2b07ad5749e2c0b22da8769e738270491e0a
Jan Redmann <janredmann1@gmail.com>
2022-01-20 11:21:15 +0100

    add testing

--- testing-intro/spec/greeting.spec.js
+++ testing-intro/spec/greeting.spec.js
@@ -1,2 +1,15 @@
 // this imports the greeting
 const greeting = require('../src/greeting')
+
+// test suite
+describe('The function greeting', function () {
+	// spec - specification 
+	it('should be a function', function () {
+		// expectation - using a matcher
+		expect(typeof greeting).toBe('function')
+	});
+	it('should return a greeting string', function () {
+		expect(greeting()).toEqual('hello ironhackers!')
+	})
+})
+
--- testing-intro/src/fizzBuzz.js
+++ testing-intro/src/fizzBuzz.js
@@ -1,3 +1,19 @@
+function fizzBuzz(number, string = '') {
+	// if (number % 15 === 0) {
+	// 	return 'fizzbuzz'
+	// }
+	// if (number % 3 === 0) {
+	// 	return 'fizz'
+	// }
+	// if (number % 5 === 0) {
+	// 	return 'buzz'
+	// }
+	// return number
+	// let string = ''
+	if (number % 3 === 0) string += 'fizz'
+	if (number % 5 === 0) string += 'buzz'
+	return string || number
+}
 
 if (typeof module !== undefined) {
 	module.exports = fizzBuzz;
